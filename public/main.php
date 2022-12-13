ea38a61184de2a9f7292aec9d0963e55dbb5eb46
Zurab Arutyunov <SpanihBob@gmail.com>
2022-12-13 15:43:54 +0400

    sauna_vaza v 1.0.1

--- public/main.php
+++ public/main.php
@@ -1,17 +1,8 @@
 <?
     $path = $_SERVER['DOCUMENT_ROOT'];
+    include_once "$path/private/head.php"
 ?>
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="/css/reset.css">
-    <link rel="stylesheet" href="/css/master.css">
-    <script src="/js/master.js" charset="utf-8"></script> <!--Скрипт работает после загрузки дом, чтоб изменить, уберите defer -->
-    <title>Document</title>
-</head>
+
 <body>
     <div class="container">
         <div class="masc2"></div>
--- public/photoandvideo.php
+++ public/photoandvideo.php
@@ -1,1 +1,15 @@
-photo
+<?
+    $path = $_SERVER['DOCUMENT_ROOT'];
+    include_once "$path/private/head.php"
+?>
+
+<body>
+    <div class="container">
+        <div class="first_photo"></div>
+    </div>
+    
+    <script>
+        
+    </script>
+</body>
+</html>
